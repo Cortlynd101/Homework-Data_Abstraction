a42d57d0d513a3374ec2afbdb7142cc6337b19c6
Cortlynd101 <CortlyndCox@gmail.com>
2021-09-05 17:03:44 -0600

    A little prep.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace HWChapTwo
 {
@@ -6,7 +7,32 @@ namespace HWChapTwo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StreamReader(); //Problem #4
+        }
+        public static void StreamReader()
+        {
+            //Problem #4
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader("TestFile.txt"))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
