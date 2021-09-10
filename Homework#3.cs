bb5efb1ad96b7fcae9f5fb5acefb6f063d08cb88
Cortlynd101 <CortlyndCox@gmail.com>
2021-09-10 16:56:00 -0600

    Finished with Homework#3.

--- Homework#3.cs
+++ Homework#3.cs
@@ -17,5 +17,53 @@ namespace AllHW
                 return start;
             }
         }
+
+        public static decimal ProblemSeven(decimal n)
+        {
+            if (n > 1)
+            {
+                return 1/n + ProblemSeven(n-1);
+            }
+            else
+            {
+                return n;
+            }
+        }
+
+        public static double power1(double x, double n) //ProblemEighteen a.
+        {
+            double newX = 1;
+            for (int i = 0; i < n; i++)
+            {
+                newX = newX * x;
+            }
+            return newX;
+        }
+        public static double power2(double x, double n) //ProblemEighteen b.
+        {
+            if (n > 0)
+            {
+                return x * power2(x, n-1);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        public static double power3(double x, double n) //ProblemEighteen c.
+        {
+            if (n > 0 & n % 2 == 0)
+            {
+                return Math.Pow(power3(x, n/2), 2);
+            }
+            else if (n > 0 & n % 2 != 0)
+            {
+                return x * Math.Pow(power3(x, n/2), 2);
+            }
+            else 
+            {
+                return 1;
+            }
+        }
     }
 }
